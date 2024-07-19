50652f98b72ddc8549b02f1c57f8d184255b59a7
Gabriel Cristian <gabriel.cristiann@gmail.com>
2024-07-18 22:37:40 -0300

    Adicionando endpoint de criação do todo

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
 def test_create_user(session):
@@ -13,3 +13,20 @@ def test_create_user(session):
 
     result = session.scalar(select(User).where(User.email == 'test@test.com'))
     assert result.username == 'testuser'
+
+
+def test_create_todo(session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
