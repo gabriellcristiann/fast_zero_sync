62f49e523de0386c37757f0f868386cc8b90d51d
Gabriel Cristian <gabriel.cristiann@gmail.com>
2024-07-22 23:40:35 -0300

    Endpoint de Alteracao

--- fast_zero/routers/todos.py
+++ fast_zero/routers/todos.py
@@ -1,11 +1,12 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import Todo, User
-from fast_zero.schemas import TodoPublic, TodoSchema
+from fast_zero.schemas import TodoList, TodoPublic, TodoSchema
 from fast_zero.security import get_current_user
 
 router = APIRouter()
@@ -29,3 +30,29 @@ def create_todo(todo: TodoSchema, user: T_CurrentUser, session: T_Session):
     session.refresh(db_todo)
 
     return db_todo
+
+
+@router.get('/', response_model=TodoList)
+def list_todos(  # noqa
+    session: T_Session,
+    user: T_CurrentUser,
+    title: str = Query(None),
+    description: str = Query(None),
+    state: str = Query(None),
+    offset: int = Query(None),
+    limit: int = Query(None),
+):
+    query = select(Todo).where(Todo.user_id == user.id)
+
+    if title:
+        query = query.filter(Todo.title.contains(title))
+
+    if description:
+        query = query.filter(Todo.description.contains(description))
+
+    if state:
+        query = query.filter(Todo.state == state)
+
+    todos = session.scalars(query.offset(offset).limit(limit)).all()
+
+    return {'todos': todos}
