5ae2487f5616a352664c8219872b2f065bf0d3a2
Gabriel Cristian <gabriel@megaconecta.com.br>
2024-08-14 22:26:36 -0300

    Dockerizando nossa aplicação e inserindo o PostgreSQL

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,8 @@ from alembic import context
 from fast_zero.settings import Settings
 from fast_zero.models import table_registry
 
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
 
